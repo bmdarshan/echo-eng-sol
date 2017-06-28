4a80a351ae3719143dfc797a737a4c9221c50367
bmdarshan <darshan.bmg@gmail.com>
2017-06-28 16:12:51 -0400

    fix router issue

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch } from "react-router";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, hashHistory } from "react-router-dom";
 
 import EchoEngHome from "./pages/home/components/echoEngHome.jsx";
 import EchoEngTeam from "./pages/team/components/echoEngTeam.jsx";
@@ -9,12 +9,11 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<BrowserRouter>
-				<Switch>
+			<div>
 					<Route path="/" component={EchoEngHome} />
-					<Route path="about" component={EchoEngHome} />
+					<Route path="/about" component={EchoEngHome} />
 					<Route path="/team" component={EchoEngTeam} />
-					<Route path="contact" component={EchoEngHome} />
-				</Switch>
+					</div>
 			</BrowserRouter>
 		);
 	}
--- client/general/components/echoHeader.jsx
+++ client/general/components/echoHeader.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default class EchoHeader extends React.Component {
 	render() {
 		return (
-			<div>
-				<NavLink to="/about">About</NavLink>
-				<NavLink to="/team">Team</NavLink>
-				<NavLink to="/contact">Contact</NavLink>
-			</div>
+			<ul>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/about">About</Link></li>
+				<li><Link to="/team">Topics</Link></li>
+			</ul>
 		);
 	}
 }
